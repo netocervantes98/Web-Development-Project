0c72f63791ad293126285e2024a2e70e6b745d57
Uriel Fuentes <urielfuentesc@gmail.com>
2020-11-03 22:01:56 -0600

    Changing data format.

--- python/filter.py
+++ python/filter.py
@@ -18,7 +18,8 @@ db = conn['price_data']
 #Schema of DB
 entry = {
     "Name": "",
-    "Registration Date": "",
+    "Year": 0.0,
+    "Month": 0.0, 
     "Price": 0.0,
     "Perc change": 0.0
 }
@@ -28,26 +29,25 @@ coll = db['data']
 
 df = pd.read_csv('INP_PP.csv', encoding = "ISO-8859-1")
 
-#Change date format
-df['Fecha_Pub_DOF'] = df['Fecha_Pub_DOF'].apply(ChnageDateFormat)
-
 #Get fields of interest
-data = df[['Generico', 'Fecha_Pub_DOF', 'Precio promedio']]
-data = data.sort_values(['Generico', 'Fecha_Pub_DOF'], ascending=False)
+data = df[['Generico', 'Year', 'Mes', 'Precio promedio']]
+data = data.sort_values(['Generico', 'Year', 'Mes'], ascending=False)
 
 #Group and get mean of duplicates
-groups = data.groupby(['Generico', 'Fecha_Pub_DOF'], sort=False)
-ResData = groups.mean().reset_index()[['Generico', 'Fecha_Pub_DOF', 'Precio promedio']]
+groups = data.groupby(['Generico', 'Year', 'Mes'], sort=False)
+ResData = groups.mean().reset_index()[['Generico', 'Year', 'Mes', 'Precio promedio']]
 
 percentage = 0.0
 rows = ResData.shape[0]
 
 ResData = ResData.round({'Precio promedio': 3})
 
+print(ResData.dtypes)
 #Calculate percentage change between records and save document to DB
 for ind in ResData.index:
     entry["Name"] = ResData['Generico'][ind]
-    entry["Registration Date"] = ResData['Fecha_Pub_DOF'][ind]
+    entry["Year"] = ResData['Year'][ind]
+    entry["Month"] = ResData['Mes'][ind]
     entry["Price"] = ResData['Precio promedio'][ind]
 
     if ind < rows-1:
@@ -64,4 +64,7 @@ for ind in ResData.index:
     
     if '_id' in entry: 
         del entry['_id'] 
+    
+    entry["Year"] = int(entry["Year"])
+    entry["Month"] = int(entry["Month"])
     coll.insert_one(entry)
